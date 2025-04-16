e572d5c13d313d8441204fa0e95faac5633d0105
zaman-shadhin <zamanshadhin26@gmail.com>
2025-04-16 21:09:13 +0600

    TicTacToe.java

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,144 +1,157 @@
-import java.util.Scanner;
+//                 SAY
+//                        MY
+//                               NAME
 
-public class TicTacToe {
-    static char[][] board = { {' ', ' ', ' '}, {' ', ' ', ' '}, {' ', ' ', ' '} };
-    static char human = 'X', ai = 'O';
+//                                         SHADHIN (24070101 cse43rd)
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.Random;
+
+public class TicTacToegame {
+    static ArrayList<Integer> playerposition = new ArrayList<>();
+    static ArrayList<Integer> cpuposition = new ArrayList<>();
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        boolean gameOn = true;
+        
+        char[][] gameboard = {{' ','|',' ','|',' '}, 
+                              {'-','+','-','+','-'},
+                              {' ','|',' ','|',' '},
+                              {'-','+','-','+','-'},
+                              {' ','|',' ','|',' '}};
+                              
+        printboard(gameboard); 
 
-        System.out.print("Do you want to play first? (yes/no): ");
-        String choice = scanner.next();
-        boolean humanTurn = choice.equalsIgnoreCase("yes");
+        while(true){
 
-        while (gameOn) {
-            printBoard();
-            if (humanTurn) {
-                humanMove(scanner);
-            } else {
-                System.out.println("AI is making a move...");
-                aiMove();
-            }
-
-            if (checkWin(human)) {
-                printBoard();
-                System.out.println("You win!");
-                break;
-            } else if (checkWin(ai)) {
-                printBoard();
-                System.out.println("AI wins!");
-                break;
-            } else if (isBoardFull()) {
-                printBoard();
-                System.out.println("It's a draw!");
+            //player movements
+            Scanner input = new Scanner(System.in);
+            System.out.println("Enter your placement (1-9):");
+            int pos =input.nextInt();
+            while(playerposition.contains(pos) || cpuposition.contains(pos) ){
+                System.out.println("This Position is taken Blind... Try the available one");
+                pos =input.nextInt();
+            } 
+            placepiece(gameboard ,pos ,"player");
+            printboard(gameboard);
+            String result = checkwinner();
+            if(result.length()>0){
+                
+                System.out.println(result);
                 break;
             }
+    
 
-            humanTurn = !humanTurn;
-        }
-
-        scanner.close();
-    }
-
-    public static void printBoard() {
-        System.out.println("-------------");
-        for (int i = 0; i < 3; i++) {
-            System.out.print("| ");
-            for (int j = 0; j < 3; j++) {
-                System.out.print(board[i][j] + " | ");
+            //cpu movements :{
+            Random rand = new Random();
+            int cpupos = rand.nextInt(9)+1;
+            while(playerposition.contains(cpupos)||cpuposition.contains(cpupos)){
+                cpupos = rand.nextInt(9)+1;
             }
-            System.out.println("\n-------------");
-        }
-    }
+            placepiece(gameboard ,cpupos ,"cpu");
+      
+            System.out.println("Cpu placement is : "+cpupos);
+            printboard(gameboard);
 
-    public static void humanMove(Scanner scanner) {
-        int row, col;
-        while (true) {
-            System.out.print("Enter row and column (0-2): ");
-            row = scanner.nextInt();
-            col = scanner.nextInt();
-            if (row >= 0 && row < 3 && col >= 0 && col < 3 && board[row][col] == ' ') {
-                board[row][col] = human;
+            result = checkwinner();
+            if(result.length()>0){
+                System.out.println(result);
                 break;
-            } else {
-                System.out.println("Invalid move. Try again.");
+            }    
+        }      
+    }
+
+    public static void printboard(char[][] gameboard){
+        for (char [] row : gameboard){
+            for(char c : row){
+                System.out.print(c);
             }
+            System.out.println( );
+        }
+    }                                      
+    public static void placepiece(char[][] gameboard,int pos, String user) {
+        
+        char symbol = 'X';
+        if(user.equals("player")){
+            symbol ='X';
+            playerposition.add(pos);
+
+        } else if(user.equals("cpu")){
+            symbol = 'O';
+            cpuposition.add(pos);
+
+        }        
+        switch(pos){
+            case 1:
+            gameboard[0][0] = symbol;
+            break;
+            case 2:
+                gameboard[0][2] = symbol;
+                break;
+             case 3:
+                gameboard[0][4] = symbol;
+                break;
+            case 4:
+                gameboard[2][0] = symbol;
+                break;
+            case 5:
+                gameboard[2][2] = symbol;
+                break;
+            case 6:
+                gameboard[2][4] = symbol;
+                break;
+            case 7:
+                gameboard[4][0] = symbol;
+                break;
+            case 8:
+                gameboard[4][2] = symbol;
+                break;
+            case 9:
+                gameboard[4][4] = symbol;
+               break;
         }
     }
 
-    public static void aiMove() {
-        int bestScore = Integer.MIN_VALUE;
-        int bestRow = -1, bestCol = -1;
+    public static String checkwinner() {
+        
+        List toprow = Arrays.asList(1,2,3);
+        List midrow = Arrays.asList(4,5,6);
+        List botrow = Arrays.asList(7,8,9);
+        List topcol = Arrays.asList(1,4,7);
+        List midcol = Arrays.asList(2,5,8);
+        List botcol = Arrays.asList(3,6,9);
+        List cross1 = Arrays.asList(1,5,9);
+        List cross2 = Arrays.asList(3,5,7);
 
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                if (board[i][j] == ' ') {
-                    board[i][j] = ai;
-                    int score = minimax(false);
-                    board[i][j] = ' ';
-                    if (score > bestScore) {
-                        bestScore = score;
-                        bestRow = i;
-                        bestCol = j;
-                    }
-                }
+        List<List> winning = new ArrayList<List>();
+        winning.add(toprow);
+        winning.add(midrow);
+        winning.add(botrow);
+        winning.add(topcol);
+        winning.add(midcol);
+        winning.add(botcol);
+        winning.add(cross1);
+        winning.add(cross2);
+
+        for(List Y : winning){
+            if(playerposition.containsAll(Y)){
+              return "Congratulations NOOB you WON :) ";   
+              }
+            else if(cpuposition.containsAll(Y)){
+                return"HAHAHA NOOB CPU WON :( ";
+            }
+            else if (playerposition.size()+cpuposition.size() == 9){
+                return "DRAW :: NICE TRY DIDDY  ";
             }
         }
-
-        board[bestRow][bestCol] = ai;
+        return "";    
     }
 
-    public static int minimax(boolean isMaximizing) {
-        if (checkWin(ai)) return 1;
-        if (checkWin(human)) return -1;
-        if (isBoardFull()) return 0;
-
-        int bestScore = isMaximizing ? Integer.MIN_VALUE : Integer.MAX_VALUE;
-
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                if (board[i][j] == ' ') {
-                    board[i][j] = isMaximizing ? ai : human;
-                    int score = minimax(!isMaximizing);
-                    board[i][j] = ' ';
-                    bestScore = isMaximizing ?
-                        Math.max(score, bestScore) :
-                        Math.min(score, bestScore);
-                }
-            }
-        }
-
-        return bestScore;
-    }
-
-    public static boolean checkWin(char player) {
-        for (int i = 0; i < 3; i++) {
-            if ((board[i][0] == player &&
-                 board[i][1] == player &&
-                 board[i][2] == player) ||
-                (board[0][i] == player &&
-                 board[1][i] == player &&
-                 board[2][i] == player)) {
-                return true;
-            }
-        }
-
-        return (board[0][0] == player &&
-                board[1][1] == player &&
-                board[2][2] == player) ||
-               (board[0][2] == player &&
-                board[1][1] == player &&
-                board[2][0] == player);
-    }
-
-    public static boolean isBoardFull() {
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                if (board[i][j] == ' ') return false;
-            }
-        }
-        return true;
-    }
 }
+// Protita line nije liksi sir you tube er help niya 
+// sir bissash koren shob nijei jene buje liksi
 
+// sir number ta ektu valo diyen sir please 
+// ei semister e COMEBACK korte hobe 
